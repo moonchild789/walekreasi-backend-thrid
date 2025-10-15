051b0c991c474f0a684929db271af2e2f9e3da9d
moonchild789 <hollylapian07@gmail.com>
2025-10-15 17:52:23 +0800

    midtrans production mode

--- helpers/midtrans.js
+++ helpers/midtrans.js
@@ -1,8 +1,8 @@
 const midtransClient = require("midtrans-client");
 
 const snap = new midtransClient.Snap({
-  isProduction: false, // ubah ke true untuk production
-  serverKey: 'SB-Mid-server-oB6N_L7x6XyGBG1BS8oW-TDt'
+  isProduction: true, // ubah ke true untuk production
+  serverKey: 'Mid-server--UVjDSp_A-fiTjcPfoDRO943'
 });
 
 module.exports = snap;
