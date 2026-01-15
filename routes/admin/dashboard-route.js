6b98d083ea72dbb51d674349608ab20b053592ab
hollypraisy <lapianholly@gmail.com>
2026-01-15 15:05:55 +0800

    perbaiki typo

--- routes/admin/dashboard-route.js
+++ routes/admin/dashboard-route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { authMiddleware, isAdmin } = require("../../controllers/auth/auth-controller");
 const { 
   getAdminDashboardStats,
-  getAdminOrderGroupedByStatus
+  getAdminOrdersGroupedByStatus
 } = require("../../controllers/admin/dashboard-controller");
 
 const router = express.Router();
@@ -11,6 +11,6 @@ const router = express.Router();
 router.get("/stats", authMiddleware, isAdmin, getAdminDashboardStats);
 
 // Data order yang dikelompokkan berdasarkan status
-router.get("/orders-by-status", authMiddleware, isAdmin, getAdminOrderGroupedByStatus);
+router.get("/orders-by-status", authMiddleware, isAdmin, getAdminOrdersGroupedByStatus);
 
 module.exports = router;
